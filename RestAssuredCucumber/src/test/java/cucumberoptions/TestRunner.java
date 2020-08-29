694177f8467d77e6043ecdea3f6aa55805f51d71
Vitthal Kulkarni <kvishal_infotech@rediffmail.com>
2020-08-30 00:54:28 +0530

    Code Modified

--- RestAssuredCucumber/src/test/java/cucumberoptions/TestRunner.java
+++ RestAssuredCucumber/src/test/java/cucumberoptions/TestRunner.java
@@ -1,14 +1,13 @@
 package cucumberoptions;
 
 import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"/RestAssuredCucumber/src/test/java/features"},
-		glue = {"/RestAssuredCucumber/src/test/java/steps"}		
+		features = "classpath:features",
+		glue = "stepdefinitions"	
 		)
 public class TestRunner {
 
--- RestAssuredCucumber/src/test/java/features/GetPost.feature
+++
@@ -1,7 +0,0 @@
-Feature: 
-  		Verify different GET operations
-
-  Scenario: Verify one author of the post
-    Given I perform GET operation for "/post"
-    And I perform GET for the post numebr "1"
-    Then I should see the author name as "Steve French"
